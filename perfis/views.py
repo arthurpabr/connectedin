88515ea7cebab39c475ffe7a612aa14a8ad33664
Arthur R Marcondes <arthur.marcondes@ifsuldeminas.edu.br>
2016-01-14 16:42:02 -0200

    Exercícios até o capítulo 9

--- perfis/views.py
+++ perfis/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.shortcuts import render
+from perfis.models import Convite
 from perfis.models import Perfil
 
 def index(request):
-	return render(request, 'index_.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
+    return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
 
 def exibir(request, perfil_id):
-	# print 'ID do perfil recebido: %s' % (perfil_id)
-	perfil = Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfil.html', {"perfil" : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    jah_eh_contato = perfil in perfil_logado.contatos.all()	
+    return render(request, 'perfil.html', {"perfil": perfil, "jah_eh_contato": jah_eh_contato})
 
 def convidar(request, perfil_id):
-	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-	perfil_logado = get_perfil_logado(request)
-	perfil_logado.convidar(perfil_a_convidar)
-	return redirect('index')
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return redirect('index')
+
+def aceitar(request, convite_id):
+    convite = Convite.objects.get(id=convite_id)
+    convite.aceitar()
+    return redirect('index')
 
 def get_perfil_logado(request):
-	return Perfil.objects.get(id=1)
+    return Perfil.objects.get(id=1)
